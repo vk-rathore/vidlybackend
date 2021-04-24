8b91504d5e4464e71024c36787f825d48ed5d914
Vishal Kumar Rathore <dragonblade2799@gmail.com>
2021-04-24 11:45:01 +0530

    backend with mongodb

---
+++ index.js
@@ -0,0 +1,23 @@
+import genres from "./routes/genres.js";
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+
+mongoose
+  .connect(
+    "mongodb+srv://dragonblade:1234@cluster0.y8nu2.mongodb.net/myFirstDatabase"
+  )
+  .then(() => console.log("connect to mongoo db ... "))
+  .catch((err) => {
+    console.error(err);
+    console.log("could not connect to mongo db ...");
+  });
+
+app.use(express.json());
+app.use("/api/genres", genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
